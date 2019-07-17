06ed8fe8b66d538046cd7b41eccbdb5150410155
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-18 05:14:36 +0530

    passted test for stock_price problem

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,9 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  pass
 
+# recipe_batches({ 'milk': 100, 'butter': 50, 'flour': 5 }, {})
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
@@ -12,3 +13,5 @@ if __name__ == '__main__':
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
